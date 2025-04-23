26f18e1352317adeb4d98404d60362247cc511af
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2025-04-23 01:19:02 -0500

    pip was changed for pip3.

--- custom_nodes/install.sh
+++ custom_nodes/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-pip install -r ComfyUI-VideoHelperSuite/requirements.txt
-pip install -r ComfyUI-ReActor/requirements.txt
-pip install -r was-node-suite-comfyui/requirements.txt
+pip3 install -r ComfyUI-VideoHelperSuite/requirements.txt
+pip3 install -r ComfyUI-ReActor/requirements.txt
+pip3 install -r was-node-suite-comfyui/requirements.txt
 
 python3 ComfyUI-ReActor/install.py
 
